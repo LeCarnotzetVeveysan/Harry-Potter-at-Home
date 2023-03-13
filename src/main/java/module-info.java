f5b66cbaa9ef194fe8b3fb5c91d19aae0eaca63d
Ghislain Demaël <58844111+LeCarnotzetVeveysan@users.noreply.github.com>
2023-03-13 11:50:27 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module rpghp.rph_potter {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens rpghp.rph_potter to javafx.fxml;
+    exports rpghp.rph_potter;
+}
